86087281190ab00e79734e2732d53e3ee4f4b310
ShauryaTheOG <business.shaurya69@gmail.com>
2024-09-17 10:43:13 +0530

    Update rascal.py

--- rascal.py
+++ rascal.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2022 speezy
+Copyright (c) 2024 Warrior
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
